6256a2a50d40e314cdaf59c6af7ed0ea12efb6d4
Théo <theo.andrist@outlook.com>
2019-04-22 22:13:31 +0200

    Initial commit

---
+++ Platform runner/Assets/Scripts/InstatiateField.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstatiateField : MonoBehaviour
+{
+
+    public GameObject Prefab;
+    public int Layers = 3;
+    public int Size = 20;
+    public Transform Parent;
+
+
+
+    private void Awake()
+    {
+        Vector3 position = new Vector3(0, 0, 0);
+
+        for (int y = 0; y < Layers; y++)
+        {
+            for (int z = 0; z < Size; z++)
+            {
+                for (int x = 0; x < Size; x++)
+                {
+                    GameObject p = Instantiate(Prefab, position, Quaternion.Euler(new Vector3(0, 0, 0)), Parent);
+                    p.name = "Platform x: " + x +" y: " + y +" z: " + z ;
+                    position += new Vector3(1.5f, 0, 0);
+                }
+                position = new Vector3(0, position.y, position.z);
+                position += new Vector3(0, 0, 1.5f);
+            }
+            position = new Vector3(0, position.y, 0);
+            position += new Vector3(0, -10, 0);
+        }
+    }
+}
---
+++ Platform runner/Assets/playerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float Speed = 15;
+    public Rigidbody Rb;
+
+    public float RaycastRange = 1f;
+
+    public float jumpForce;
+
+    public float verticalVelocity;
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+
+        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            Rb.AddForce(0, jumpForce * Time.deltaTime, 0, ForceMode.Impulse);
+        }
+        else if (IsGrounded())
+        {
+            Rb.useGravity = false;
+            Rb.velocity = new Vector3(0, 0, Rb.velocity.z);
+        }
+        else
+        {
+            Rb.useGravity = true;
+        }
+
+        Rb.AddForce(0, verticalVelocity, Speed * Time.deltaTime, ForceMode.VelocityChange);
+    }
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, RaycastRange);
+    }
+}
