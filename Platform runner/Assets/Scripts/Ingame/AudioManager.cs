fe58bd6b9464bae9209bdccf064f58d20016c763
Giguk <47025909+Giguk@users.noreply.github.com>
2019-04-23 16:53:13 +0200

    Menu Sound und ein wenig Ordnung

---
+++ Platform runner/Assets/Scripts/Ingame/AudioManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    public AudioMixer Mixer;
+
+    public static AudioManager audioInstance;
+
+    public GameObject AudioPanel;
+
+    public Slider gesamtSlider;
+    public Slider musikSlider;
+    public Slider soundSlider;
+
+    private void Awake()
+    {
+        if (audioInstance == null)
+        {
+            audioInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+    }
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+
+        DontDestroyOnLoad(gameObject);
+
+        AudioPanel = GameObject.Find("Canvas/AudioPanel");
+        AudioPanel.SetActive(false);
+
+        gesamtSlider = AudioPanel.transform.GetChild(2).GetComponent<Slider>();
+        musikSlider = AudioPanel.transform.GetChild(4).GetComponent<Slider>();
+        soundSlider = AudioPanel.transform.GetChild(6).GetComponent<Slider>();
+
+        gesamtSlider.onValueChanged.AddListener(setVolumeGesamt);
+        musikSlider.onValueChanged.AddListener(setVolumeMusik);
+        soundSlider.onValueChanged.AddListener(setVolumeSound);
+    }
+
+    public void setVolumeGesamt(float volume)
+    {
+        Mixer.SetFloat("GesamtVolume", volume);
+    }
+
+    public void setVolumeMusik(float volume)
+    {
+        Mixer.SetFloat("MusikVolume", volume);
+    }
+
+    public void setVolumeSound(float volume)
+    {
+        Mixer.SetFloat("SoundVolume", volume);
+    }
+}
---
+++ Platform runner/Assets/Scripts/Ingame/Sound.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine.Audio;
+using UnityEngine.UI;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound  {
+
+    public string name;
+
+    public AudioClip clip;
+
+    [HideInInspector]
+    public AudioSource source;
+
+
+}
