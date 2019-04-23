50b0317ec6d90e5ce594614e2fac8edce3769c3b
Giguk <47025909+Giguk@users.noreply.github.com>
2019-04-23 10:00:47 +0200

    Player Jump

--- Platform runner/Assets/Scripts/playerMovement.cs
+++ Platform runner/Assets/Scripts/playerMovement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class playerMovement : MonoBehaviour
 {
     public float Speed = 15;
@@ -11,16 +12,43 @@ public class playerMovement : MonoBehaviour
 
     public float jumpForce;
 
-    public float verticalVelocity;
-    // Update is called once per frame
+    
+
+    public float gravityScale = 1.0f;
+    public static float globalGravity = 19.81f;
+    
+    public bool jumpTrue;
+    public float Timer;
+    private float Countdown; 
+
+    void Start() {
+        //Countdown = Timer;
+
+    }
+   
+    void Update(){
+        if (Input.GetKeyDown(KeyCode.Space) && Countdown <= 0) {
+            jumpTrue = true;
+            Countdown = Timer;
+        }
+        if (Countdown > 0) {
+            Countdown -= Time.deltaTime;
+        }
+        else {
+            jumpTrue = false;
+            
+        }
+    }
     void FixedUpdate()
     {
-        
+         Vector3 gravity = globalGravity * gravityScale * Vector3.down;
+         
 
-        if (IsGrounded() && Input.GetKeyDown(KeyCode.Space))
+        if (IsGrounded() && jumpTrue == true)
         {
             Rb.AddForce(0, jumpForce * Time.deltaTime, 0, ForceMode.Impulse);
         }
+        /* 
         else if (IsGrounded())
         {
             Rb.useGravity = false;
@@ -29,9 +57,10 @@ public class playerMovement : MonoBehaviour
         else
         {
             Rb.useGravity = true;
-        }
+        }*/
 
-        Rb.AddForce(0, verticalVelocity, Speed * Time.deltaTime, ForceMode.VelocityChange);
+        //Rb.AddForce(0, verticalVelocity, Speed * Time.deltaTime, ForceMode.VelocityChange);
+        Rb.AddForce(gravity, ForceMode.Acceleration);
     }
     bool IsGrounded()
     {
