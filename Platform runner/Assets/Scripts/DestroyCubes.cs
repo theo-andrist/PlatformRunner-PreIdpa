0a1ae00251bf74f1e13273c0d4296f63811abd12
Théo Bernard Andrist <tan109713@SN-SRV.IET-GIBB.CH>
2019-04-23 10:00:19 +0200

    Cube Scripts

---
+++ Platform runner/Assets/Scripts/DestroyCubes.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCubes : MonoBehaviour
+{     
+    void OnCollisionExit(Collision other)
+    {
+        if(other.transform.tag == "Cube")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
