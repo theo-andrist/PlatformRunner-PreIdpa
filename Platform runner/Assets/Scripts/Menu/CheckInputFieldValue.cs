ff44a9506012edefdfaca7e0f472b7c7f8fffbe4
Théo Bernard Andrist <tan109713@SN-SRV.IET-GIBB.CH>
2019-04-24 14:08:27 +0200

    MainMenu Playerprefs

---
+++ Platform runner/Assets/Scripts/Menu/CheckInputFieldValue.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CheckInputFieldValue : MonoBehaviour
+{    
+    bool inputCorrect = false;
+    int value;
+
+    public void checkInput(string text){
+        if(int.TryParse(text, out value)){
+            if(value <= 3 || value > 0){
+                inputCorrect = true;
+            }
+        }
+        else if (text == "") {
+            inputCorrect = true;
+        }
+    if (!inputCorrect) {
+        text = "";
+    }
+    }
+    
+}
