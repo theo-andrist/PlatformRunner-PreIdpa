f191588fbd2df87664a4ffad87c2d700681ec257
Théo Bernard Andrist <tan109713@SN-SRV.IET-GIBB.CH>
2019-04-24 09:23:06 +0200

    scene

---
+++ Platform runner/Assets/Scripts/BumpAway.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumpAway : MonoBehaviour
+{
+    public float height;
+    public float distance;
+    //private RigidBody rb;
+
+    void OnCollisionEnter(Collision other){
+        if (other.gameObject.tag == "Enemy" || other.gameObject.name == "Player" ){
+            /* rb = other.gameObject.GetComponent<Rigidbody>();
+            rb.addForce(0, height, 0, ForceMode.Impulse);
+            rb.addForce(tranform.forward * distance, ForceMode.Impulse);*/
+        }
+    }
+}
