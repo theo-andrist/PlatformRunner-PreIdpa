7fd93bc4ca2404c51fbf842441a35e6a17e2ba0e
Théo <theo.andrist@outlook.com>
2019-04-29 19:50:22 +0200

    Soundslider bug fixed + menu- und soundpanel schöner

---
+++ Platform runner/Assets/AddListenerToEvent.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AddListenerToEvent : MonoBehaviour
+{
+    public audioManager aM;
+    public Slider slider;
+
+    public bool masterVolume;
+    public bool soundVolume;
+    public bool musikVolume;
+
+    private void Awake()
+    {
+        if (masterVolume)
+        {
+            aM = GameObject.FindObjectOfType<audioManager>();
+            slider.onValueChanged.AddListener(aM.setMasterVolume);
+        }
+        else if (musikVolume)
+        {
+            aM = GameObject.FindObjectOfType<audioManager>();
+            slider.onValueChanged.AddListener(aM.setMusicVolume);
+        }
+        else if (soundVolume)
+        {
+            aM = GameObject.FindObjectOfType<audioManager>();
+            slider.onValueChanged.AddListener(aM.setSoundVolume);
+        }
+    }
+}
